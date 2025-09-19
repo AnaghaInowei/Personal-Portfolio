108b6a19d1a8fba70b7fef2ceaae4c3352dfc73c
Ino Tech <ezekieldavid150@gmail.com>
2025-09-19 23:42:28 +0100

    style: update Header component for improved navigation and styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <nav className="header">
-      <h1 className="logo">{"<InoweiAnagha />"}</h1>
+  <h1 className="logo">&lt;Inowei<span className="name">Anagha</span> /&gt;</h1>
       <ul className="nav-links">
         <li>Home</li>
         <li>About</li>
