49d782882459cb1ba5713282752cce1f3477794d
Ino Tech <ezekieldavid150@gmail.com>
2025-09-19 23:33:34 +0100

    refactor: restructure App component and add Header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <nav className="header">
+      <h1>{"<InoweiAnagha />"}</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Techstack</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
