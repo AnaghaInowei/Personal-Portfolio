6023e0cef509e748222d2fe17e07dbf317cbf176
Ino Tech <ezekieldavid150@gmail.com>
2025-09-20 01:06:40 +0100

    style: update Header component for improved styling and add class names for better structure

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
     <header>
-      <p>Hello I'm,</p>
+      <p className="greeting">Hello I'm,</p>
       <h1>Inowei Anagha</h1>
-      <p>Front-end Web developer</p>
-      <p>I craft responsive websites where technology meets creativity</p>
+      <p className="work">Front-End Web Developer</p>
+      <p className="txt">I craft responsive websites where technology meets creativity</p>
     </header>
   );
 }
