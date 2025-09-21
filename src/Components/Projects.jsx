300973ff78033ec245471a3d9d03be56a5ea28a6
Ino Tech <ezekieldavid150@gmail.com>
2025-09-22 00:29:38 +0100

    style: update card backgrounds and text alignment for improved aesthetics; add project images and technology tags

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -19,14 +19,26 @@ export default function Projects() {
             <p>A project completed during the Techyjaunt internship program</p>
             <a href="#">View Project</a>
           </div>
-          <div className="card">  
-            <h3>Project Title 2</h3>
-            <p>A brief description of Project 2, highlighting its features and technologies used.</p>
+          <div className="card">
+            <img src="./src/assets/RentDirect.png" alt="Rent Direct Landing Page" width={100} />
+            <div className="tech">
+              <span>HTML</span>
+              <span>CSS</span>
+              <span>JavaScript</span>
+            </div>
+            <h3>Rent Direct</h3>
+            <p>A project completed during the Techyjaunt internship program</p>
             <a href="#">View Project</a>
           </div>
           <div className="card">
-            <h3>Project Title 3</h3>
-            <p>A brief description of Project 3, highlighting its features and technologies used.</p>
+            <img src="./src/assets/RentDirect.png" alt="Rent Direct Landing Page" width={100} />
+            <div className="tech">
+              <span>HTML</span>
+              <span>CSS</span>
+              <span>JavaScript</span>
+            </div>
+            <h3>Rent Direct</h3>
+            <p>A project completed during the Techyjaunt internship program</p>
             <a href="#">View Project</a>
           </div>
         </div>
